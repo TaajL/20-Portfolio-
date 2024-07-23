abfb548f40daad9b7456a4d55fe12314fd151fd3
Malcom X <Taajlogan@gmail.com>
2024-07-23 13:10:04 -0400

    router working Properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // pages
-import App from "./app.jsx";
-import ErrorPage from "./pages/errorPage.jsx";
-import HomePage from "./pages/homePage.jsx";
-
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import AboutMePage from "./pages/AboutMePage.jsx";
+import PortfolioPage from "./pages/PortfolioPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import ResumePage from "./pages/ResumePage.jsx";
 // router
 const router = createBrowserRouter([
   {
@@ -15,6 +18,22 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
+      {
+        path: 'about',
+        element: <AboutMePage />
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />
+      }
     ],
   },
 ]);
